81e00ed741ccd7dd266f8477a9556b0545a6c3b1
Joshua Evans <TheJoshuaEvans@gmail.com>
2018-10-19 12:43:24 -0700

    Trifoia static Web Submodule - Made trifoia-static-web template submodule

--- src/templates/trifoia-static-web/config/scss_options.js
+++
@@ -1,8 +0,0 @@
-/**
- * Options passed to scss when rendering
- */
-module.exports = {
-  // Determines the output format of the final CSS style.
-  // Values: nested, expanded, compact, compressed
-  outputStyle: 'compressed'
-};
